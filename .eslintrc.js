dd41d5d77aa889b1bf7457030c2a1520f58bf7e9
Andy Ainsworth <andy.ainsworth@zeninternet.co.uk>
2017-12-12 16:15:11 +0000

    Remove unused files. Setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "airbnb-base",    
+    "plugins": [
+        "import"
+    ],
+    "rules": {
+        "semi": [2, "never"],
+        "linebreak-style": ["error", "windows"],
+        "indent": [ 2, 4 ],
+        "eol-last": [ "error", "never"],
+        "function-paren-newline": ["error", "never"],
+    }
+}
