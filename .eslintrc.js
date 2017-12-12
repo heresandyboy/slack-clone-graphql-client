e92b0ffefa35eb318448e65d8ae259f160c15ef1
Andy Ainsworth <andy.ainsworth@zeninternet.co.uk>
2017-12-12 16:55:07 +0000

    Better eslint config for React

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,34 @@
 module.exports = {
-    "extends": "airbnb-base",    
     "plugins": [
-        "import"
+        "react"
     ],
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "env": {
+        "es6":     true,
+        "browser": true,
+        "node":    true,
+        "mocha":   true
+    },   
+    "extends": [
+        "eslint:recommended", 
+        "plugin:react/recommended"
+    ],
+    // "extends": "airbnb-base",      
     "rules": {
         "semi": [2, "never"],
         "linebreak-style": ["error", "windows"],
-        "indent": [ 2, 4 ],
-        "eol-last": [ "error", "never"],
+        // "indent": [2, 4],
+        "eol-last": ["error", "never"],
         "function-paren-newline": ["error", "never"],
+        "no-use-before-define": ["error", {
+            "functions": false,
+            "classes": true
+        }]
     }
 }
