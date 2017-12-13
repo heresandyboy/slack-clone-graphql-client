6009e7f071597013a55122165f410d6f7d2e9f92
Andy Ainsworth <andy.ainsworth@zeninternet.co.uk>
2017-12-13 17:26:14 +0000

    Feature: implement server side validation error messages in Register component

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,15 +3,17 @@ import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
 
 const Home = ({ data: { loading, allUsers } }) =>
-    (loading ? null : allUsers.map(u => <h1 key={u.id}>{u.email}</h1>))
+    (loading ? null : allUsers.map(u =>
+        <h1 key={u.id}>{u.username} : {u.email}
+        </h1>))
 
 const allUsersQuery = gql`
  {   
      allUsers {
         id
+        username
         email        
     }
  }
 `
-
 export default graphql(allUsersQuery)(Home)
--- src/routes/Register.js
+++ src/routes/Register.js
@@ -2,27 +2,50 @@ import React, { Component } from 'react'
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
 import PropTypes from 'prop-types'
-import { Container, Header, Input, Button } from 'semantic-ui-react'
+import { Container, Header, Input, Button, Message } from 'semantic-ui-react'
 
 class Register extends Component {
     state = {
         username: '',
+        usernameError: '',
         email: '',
+        emailError: '',
         password: '',
+        passwordError: ''
     }
-    
+
     static propTypes = {
         mutate: PropTypes.func.isRequired,
-     }
+        history: PropTypes.object,
+    }
 
     onSubmit = async () => {
-        const response = await this.props.mutate({
-          variables: this.state,
+
+        this.setState({
+            usernameError: '',
+            emailError: '',
+            passwordError: ''
         })
-        
-        console.log(this.state)
-        console.log(response)
-      }
+
+        const { username, email, password } = this.state
+
+        const response = await this.props.mutate({
+            variables: { username, email, password },
+        })
+
+        const { ok, errors } = response.data.register
+
+        if (ok) {
+            this.props.history.push('/')
+        } else {
+            const validationError = {}
+            errors.forEach(({ path, message }) => {
+                validationError[`${path}Error`] = message
+            })
+
+            this.setState(validationError)
+        }
+    }
 
     onChange = e => {
         const { name, value } = e.target
@@ -31,15 +54,42 @@ class Register extends Component {
 
     render() {
 
-        const { username, email, password } = this.state
+        const { username, email, password, usernameError, emailError, passwordError } = this.state
+
+        const errorList = []
+
+        if (usernameError) errorList.push(usernameError)
+        if (emailError) errorList.push(emailError)
+        if (passwordError) errorList.push(passwordError)
 
         return (
             <Container text>
                 <Header as="h2">Register</Header>
-                <Input name="username" onChange={this.onChange} value={username} placeholder="Username" fluid />
-                <Input name="email" onChange={this.onChange} value={email} placeholder="Email" fluid />
-                <Input name="password" onChange={this.onChange} value={password} type="password" placeholder="Password" fluid />
+                <Input name="username"
+                    value={username}
+                    placeholder="Username" fluid
+                    onChange={this.onChange}
+                    error={!!usernameError}
+                />
+                <Input name="email"
+                    value={email}
+                    placeholder="Email" fluid
+                    onChange={this.onChange}
+                    error={!!emailError}
+                />
+                <Input name="password"
+                    value={password} type="password"
+                    placeholder="Password" fluid
+                    onChange={this.onChange}
+                    error={!!passwordError}
+                />
                 <Button onClick={this.onSubmit}>Submit</Button>
+                {(usernameError || passwordError || emailError) ? (<Message
+                    error
+                    header='There are some errors with your submission'
+                    list={errorList}
+                    />
+                ) : null}          
             </Container>
         )
     }
@@ -47,7 +97,13 @@ class Register extends Component {
 
 const registerMutation = gql`
     mutation($username: String!, $email: String!, $password: String!) {
-	    register(username: $username, email: $email, password: $password)
+        register(username: $username, email: $email, password: $password) {
+            ok
+            errors {
+                path
+                message
+            }
+        }
     }
 `
 
