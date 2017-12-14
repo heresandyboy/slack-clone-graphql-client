346701e1d362bf56cad26e5b0144562e5a6fdf5b
Andy Ainsworth <andy.ainsworth@zeninternet.co.uk>
2017-12-14 10:08:49 +0000

    Added Login form, using Mobx as an alternative to local state or redux

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,7 +28,7 @@ module.exports = {
         "linebreak-style": ["error", "windows"],
         // "indent": [2, 4],
         "eol-last": ["error", "never"],
-        "function-paren-newline": ["error", "never"],
+        // "function-paren-newline": ["error", "multiline"],
         "no-use-before-define": ["error", {
             "functions": false,
             "classes": true
