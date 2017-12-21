a0d31815e3d72b72a20a70f253bd5538c1a1ce25
Andy Ainsworth <andy.ainsworth@zeninternet.co.uk>
2017-12-21 16:47:07 +0000

    Feature: Send messages and setup GraphQL  subscriptions

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,21 +1,73 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { Input } from 'semantic-ui-react'
-import PropTypes  from 'prop-types'
+import { withFormik } from 'formik'
+import gql from 'graphql-tag'
+import { compose, graphql } from 'react-apollo'
+
 
 const SendMessageStyle = styled.div`
-  grid-column: 3;
-  grid-row: 3;
-  margin: 20px;
+grid-column: 3;
+padding: 20px;
 `
 
-const SendMessage = ({ channelName }) => (
+
+const ENTER_KEY = 13
+
+const SendMessage = ({
+  channelName,
+  values,
+  handleChange,
+  handleBlur,
+  handleSubmit,
+  isSubmitting,
+}) => (
   <SendMessageStyle>
-    <Input fluid placeholder={`Message #${channelName}`} />
+    <Input
+      onKeyDown={(e) => {
+        if (e.keyCode === ENTER_KEY && !isSubmitting) {
+          handleSubmit(e)
+        }
+      }}
+      onChange={handleChange}
+      onBlur={handleBlur}
+      name="message"
+      value={values.message}
+      fluid
+      placeholder={`Message #${channelName}`}
+    />
   </SendMessageStyle>
 )
 SendMessage.propTypes = {
-  channelName: PropTypes.string
+  channelName: PropTypes.string,
+  values: PropTypes.any,
+  handleChange: PropTypes.any,
+  handleBlur: PropTypes.any,
+  handleSubmit: PropTypes.any,
+  isSubmitting: PropTypes.any
 }
 
-export default SendMessage
+const createMessageMutation = gql`
+  mutation($channelId: Int!, $text: String!) {
+    createMessage(channelId: $channelId, text: $text)
+  }
+`
+
+export default compose(
+  graphql(createMessageMutation),
+  withFormik({
+    mapPropsToValues: () => ({ message: '' }),
+    handleSubmit: async (values, { props: { channelId, mutate }, setSubmitting, resetForm }) => {
+      if (!values.message || !values.message.trim()) {
+        setSubmitting(false)
+        return
+      }
+
+      await mutate({
+        variables: { channelId, text: values.message },
+      })
+      resetForm(false)
+    },
+  }),
+)(SendMessage)
